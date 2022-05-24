94f67d8ecd949c675fd20a5493e05777845515c0
Jami Beltre Casado <beltrej01@gmail.com>
2022-05-24 16:26:57 +0200

    first commit

---
+++ funciones.js
@@ -0,0 +1,12 @@
+//Inicializamos AOS
+//AOS.init();
+
+const menu = document.getElementById("menu");
+const ham = document.getElementById("menu-bar");
+
+ham.addEventListener("click", ()=> {
+
+menu.classList.toggle("menu-toggle")
+
+}
+)
